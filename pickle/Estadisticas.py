bb5d29820924be452bc2490d94dffff11624d9c6
up201605590@fe.up.pt <up201605590@fe.up.pt>
2019-09-11 00:30:09 +0100

    estatistica added params

--- pickle/Estadisticas.py
+++ pickle/Estadisticas.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
+tipo = 1
+index_image = 2
+
+
 # ========== EXTRACCIÓN DE DATOS ==============
 archivo = open('/home/endredra/Desktop/Int_proj/EL4106-1oProyecto/dataset/HiTS2013_100k_samples(4_channels)_images_labels.pkl',"rb")
 example_dict = pickle.load(archivo)
@@ -10,7 +14,7 @@ print()
 print(example_dict['images'].shape)
 
 print()
-xx = example_dict['images'][:, :, :, 1]
+xx = example_dict['images'][:, :, :, tipo]
 print(xx[0].shape)
 
 #x = np.zeros((2, 3, 4, 4))
@@ -30,7 +34,7 @@ print(xx[0].shape)
 #print(x.shape[1])
 #print()
 
-x = xx[23]
+x = xx[index_image]#[23]
 plt.imshow(x)
 plt.show()
 print()
